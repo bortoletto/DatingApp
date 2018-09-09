858bdc85a2c53ce2c76551e206c931e4803be5cd
Albert <albert.bortoletto@hotmail.com>
2018-09-09 15:25:19 -0300

    End of Section 4

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -8,7 +8,7 @@ namespace DatingApp.API.Dtos
         public string UserName { get; set; }
         
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characteres.")]
+        [StringLength(8, MinimumLength = 3, ErrorMessage = "You must specify password between 4 and 8 characteres.")]
         public string Password { get; set; }
     }
 }
